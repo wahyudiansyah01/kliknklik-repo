26370f7d50dec01f9fe715a55461c30b88e4952b
wahyudiansyah01 <wahyudiansyah.arif@yahoo.co.id>
2016-01-12 14:57:25 +0700

    Create menu449.js

---
+++ htmlbox pro/arif/js/menu449.js
@@ -0,0 +1,16 @@
+ var menu449 = new Menu(null);
+        var node = document.getElementById("foto1").getElementsByTagName("a");
+        //mengambil link sebagai id
+       for (i = 0; i < node.length; i++) {
+           console.log(i);
+           var padd = node[i].parentNode.style.paddingLeft;
+           var id = node[i].getAttribute("href").match(/\d+/)[0];
+           
+           if(padd==="8px"){
+           eval("var menu"+id+" = new Menu(menu449,node[i].parentNode.parentNode)");
+           }
+           if(padd==="40px"){
+           eval("var menu"+id+" = new Menu(menu449.child[menu449.child.length - 1],node[i].parentNode.parentNode)");
+           }
+           }
+           console.log(node);
