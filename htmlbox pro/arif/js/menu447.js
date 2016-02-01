34763197886412fd1a7f5990fecd05473f5de6b2
wahyudiansyah01 <wahyudiansyah.arif@yahoo.co.id>
2016-02-01 14:41:03 +0700

    Create menu447.js

---
+++ htmlbox pro/arif/js/menu447.js
@@ -0,0 +1,19 @@
+ var menu447 = new Menu(null);
+        var node = document.getElementById("foto1").getElementsByTagName("a");
+        //mengambil link sebagai id
+       for (i = 0; i < node.length; i++) {
+           console.log(i);
+           var padd = node[i].parentNode.style.paddingLeft;
+           var id = node[i].getAttribute("href").match(/\d+/)[0];
+           
+           if(padd==="8px"){
+           eval("var menu"+id+" = new Menu(menu447,node[i].parentNode.parentNode)");
+           }
+           if(padd==="40px"){
+           eval("var menu"+id+" = new Menu(menu447.child[menu447.child.length - 1],node[i].parentNode.parentNode)");
+           }
+           if(padd==="80px"){
+           eval("var menu"+id+" = new Menu(menu447.child[menu447.child.length - 1].child[menu447.child[menu447.child.length - 1].child.length - 1],node[i].parentNode.parentNode)");
+           }
+           }
+           console.log(node);
